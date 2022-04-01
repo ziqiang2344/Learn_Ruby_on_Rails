22960b4eb23ca34fc8fd1f49c5d00ad9aa12c685
Lim Zi Qiang <qiang@gmail.com>
2022-04-01 15:40:49 +0800

    feat: update 2.4_comparison.rb with codes

---
+++ sec2_the_ruby_programming_language/2.4_comparison.rb
@@ -0,0 +1,16 @@
+puts 10 == 9            #false
+puts 10 == "10".to_i    #true
+puts 10 == "10".to_f    #true
+puts 10 != 9            #true
+puts "hello" != "bye"   #true
+puts "hello" != "hello" #false
+puts "hello" == "hello" #true
+puts 100>99             #true
+puts 100>=100           #true
+puts 100>= 99           #true
+puts 100>= 101          #false
+puts 100 <= 101         #true
+puts "" == ""           #true
+
+puts 10 === 10          #true
+puts 10.eql?(10.0)      #false
