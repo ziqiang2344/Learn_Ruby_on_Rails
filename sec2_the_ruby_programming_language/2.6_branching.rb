2e36bf350dda5aede0ec11808907914b7ddb3723
Lim Zi Qiang <qiang@gmail.com>
2022-04-01 16:44:42 +0800

    feat: upload 2.6_branching.rb with codes

---
+++ sec2_the_ruby_programming_language/2.6_branching.rb
@@ -0,0 +1,20 @@
+puts "Simple calculator"
+20.times { print "-" }
+puts
+puts "Please enter your first number"
+first_number = gets.chomp
+puts "Please enter your second number"
+second_number = gets.chomp
+puts "What do you want to do?"
+puts "Enter 1 for multiply, 2 for addition, 3 for subtraction"
+
+user_entry = gets.chomp
+if user_entry == "1"
+  puts "You have chosen to multiply"
+elseif user_entry == "2"
+  puts "You have chosen to add"
+elseif user_entry == "3"
+  puts "You have chosen to subtract"
+else
+  puts "Invalid entry"
+end
