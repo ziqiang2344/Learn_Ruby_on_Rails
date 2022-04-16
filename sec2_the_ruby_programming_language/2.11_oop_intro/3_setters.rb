e1b66367419975d54c3e0ed1457c5a0bf797fdfb
Lim Zi Qiang <qiang@gmail.com>
2022-04-16 18:11:05 +0800

    feat(3_setters.rb): Learn setters

---
+++ sec2_the_ruby_programming_language/2.11_oop_intro/3_setters.rb
@@ -0,0 +1,20 @@
+class Student
+    @first_name
+    @last_name
+    @email
+    @username
+    @password
+
+    def first_name=(name) #getter function
+      @first_name = name
+    end
+
+    def to_s
+      "First name: #{@first_name}"
+    end
+
+end
+
+qiang = Student.new
+qiang.first_name = "qiang"
+puts qiang #First name: qiang
