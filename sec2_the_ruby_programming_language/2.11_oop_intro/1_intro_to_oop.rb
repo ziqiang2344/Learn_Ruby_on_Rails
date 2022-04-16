d255b2a429ee0c03f733e1a248e41d9220d364ef
Lim Zi Qiang <qiang@gmail.com>
2022-04-16 18:05:57 +0800

    feat(1_intro_to_oop.rb): Learn OOP basics

---
+++ sec2_the_ruby_programming_language/2.11_oop_intro/1_intro_to_oop.rb
@@ -0,0 +1,19 @@
+#Introduction to Object Oriented Programming
+class User
+  attr_accessor :name, :email
+  def initialize(name,email)
+    @name = name
+    @email = email
+  end
+
+  def run
+    puts "Hey I'm running"
+  end
+  #The instance of the user is not needed to run this class method
+  def self.identify_yourself
+    puts "Hey I am a class method"
+  end
+end
+ user = User.new("ziqiang", "ziqiang@example.com")
+ user.run                   #Hey I'm running
+ User.identify_yourself     #Hey I am a class method
