36110c60bd32871ca7bbc21a09b5217984820e4d
Lim Zi Qiang <qiang@gmail.com>
2022-04-30 15:59:27 +0800

    fix(2.8_hashes.rb): standardising the spaces around comma

--- sec2_the_ruby_programming_language/2.8_hashes.rb
+++ sec2_the_ruby_programming_language/2.8_hashes.rb
@@ -1,7 +1,7 @@
 #Hashes a.k.a.dictionary
 
 samples_hash = {'a' => 1, 'b' => 2, 'c' => 3}
-my_details = {'name' => 'mashrur','favcolor' => 'red'}
+my_details = {'name' => 'mashrur', 'favcolor' => 'red'}
 p my_details['favcolor']  #"red"
 p samples_hash['b']       #2
 p samples_hash            #{"a"=>1, "b"=>2, "c"=>3}
@@ -32,5 +32,5 @@ p myhash #{:a=>1, :b=>2, :c=>3, :d=>4, :e=>"Mashrur"}
 myhash[:c] = "Ruby"
 p myhash #{:a=>1, :b=>2, :c=>"Ruby", :d=>4, :e=>"Mashrur"}
 myhash.each {|some_key, some_value| puts "The key is #{some_key} and the value is #{some_value}"}
-myhash.each {|k,v| myhash.delete(k) if v.is_a?(String)}
+myhash.each {|k, v| myhash.delete(k) if v.is_a?(String)}
 p myhash #{:a=>1, :b=>2, :d=>4}
