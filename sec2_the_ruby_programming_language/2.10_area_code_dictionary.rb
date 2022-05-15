28c36d52e6b32ad49e17ec229eb62d89010468a3
ziqiang2344 <72864300+ziqiang2344@users.noreply.github.com>
2022-05-15 22:44:48 +0800

    Update sec2_the_ruby_programming_language/2.10_area_code_dictionary.rb
    Co-authored-by: Kiong <kiong90@gmail.com>

--- sec2_the_ruby_programming_language/2.10_area_code_dictionary.rb
+++ sec2_the_ruby_programming_language/2.10_area_code_dictionary.rb
@@ -1,15 +1,15 @@
 dial_book = {
-    "newyork" => "212",
-    "newbrunswick" => "732",
-    "edison" => "908",
-    "plainsboro" => "609",
-    "sanfrancisco" => "301",
-    "miami" => "305",
-    "paloalto" => "650",
-    "evanston" => "847",
-    "orlando" => "407",
-    "lancaster" => "717"
-  }
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
    
 # Get city names from the hash
 def get_city_names(somehash)
