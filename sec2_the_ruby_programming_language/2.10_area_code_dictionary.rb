27ddaac5d966b4d87274339a048270b9de6d25b6
ziqiang2344 <72864300+ziqiang2344@users.noreply.github.com>
2022-05-15 22:44:59 +0800

    Update sec2_the_ruby_programming_language/2.10_area_code_dictionary.rb
    Co-authored-by: Kiong <kiong90@gmail.com>

--- sec2_the_ruby_programming_language/2.10_area_code_dictionary.rb
+++ sec2_the_ruby_programming_language/2.10_area_code_dictionary.rb
@@ -25,7 +25,7 @@ end
 loop do
   puts "Do you want to lookup an area code based on a city name?[Y/N]"
   answer = gets.chomp.downcase
-  break if answer != "y"
+  break if answer.downcase != "y"
   puts "Which city do you want to lookup the areacode for?"
   puts get_city_names(dial_book)
   puts "Enter your selection"
